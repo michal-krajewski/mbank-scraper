ec3d2e8c3f8173ae98eb6d8669899127a9a2bf48
Michał Krajewski <Krajewski.M@protonmail.com>
2020-07-15 16:17:57 +0200

    Removed MockUserInput class

--- src/test/java/pl/byteit/mbankscraper/util/UserInterfaceTest.java
+++ src/test/java/pl/byteit/mbankscraper/util/UserInterfaceTest.java
@@ -2,16 +2,17 @@ package pl.byteit.mbankscraper.util;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import pl.byteit.mbankscraper.MockUserInput;
+import org.mockito.Mock;
 import pl.byteit.mbankscraper.operation.AccountInfo;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.data.Index.atIndex;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 import static pl.byteit.mbankscraper.operation.AccountInfo.AccountType.SAVING;
 import static pl.byteit.mbankscraper.operation.AccountInfo.AccountType.STANDARD;
 
@@ -19,15 +20,16 @@ class UserInterfaceTest {
 
 	private static final String MOCKED_INPUT = "mocked-input";
 
-	private static List<String> mockPrinter;
-	private final MockUserInput mockUserInput = new MockUserInput();
+	@Mock
+	private static List<String> mockIO;
+
 	private UserInterface cli;
 
 	@BeforeEach
 	void beforeEachCommandLineInterfaceTest() {
-		mockPrinter = new ArrayList<>();
-		mockUserInput.reset();
-		cli = new UserInterface(mockUserInput::getInput, msg -> mockPrinter.add(msg));
+		initMocks(this);
+		mockIO.clear();
+		cli = new UserInterface(() -> mockIO.get(0), msg -> mockIO.add(msg));
 	}
 
 	@Test
@@ -36,9 +38,7 @@ class UserInterfaceTest {
 
 		cli.print(printedMessage);
 
-		assertThat(mockPrinter)
-				.hasSize(1)
-				.contains(printedMessage, atIndex(0));
+		verify(mockIO).add(printedMessage);
 	}
 
 	@Test
@@ -47,9 +47,7 @@ class UserInterfaceTest {
 
 		cli.print(account);
 
-		assertThat(mockPrinter)
-				.hasSize(1)
-				.contains(account.print(), atIndex(0));
+		verify(mockIO).add(account.print());
 	}
 
 	@Test
@@ -59,26 +57,21 @@ class UserInterfaceTest {
 
 		cli.print(asList(account, account2));
 
-		assertThat(mockPrinter)
-				.hasSize(2)
-				.contains(account.print(), atIndex(0))
-				.contains(account2.print(), atIndex(1));
+		verify(mockIO).add(account.print());
+		verify(mockIO).add(account2.print());
 	}
 
 	@Test
 	void shouldPromptForInput() {
-		mockUserInput.mockInput(MOCKED_INPUT);
+		when(mockIO.get(0)).thenReturn(MOCKED_INPUT);
 		String printedMessage = "Provide input";
 
 		char[] input = cli.promptForInput(printedMessage);
 
-		assertThat(mockPrinter)
-				.hasSize(1)
-				.contains(printedMessage, atIndex(0));
+		verify(mockIO).add(printedMessage);
+		verify(mockIO).get(0);
 		assertThat(input)
 				.containsExactly(MOCKED_INPUT.toCharArray());
-		assertThat(mockUserInput.getInvocationAmount())
-				.isEqualTo(1);
 	}
 
 }
